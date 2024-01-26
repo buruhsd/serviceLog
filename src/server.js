0c1bccc46604de85909a61fc7b18c6af33fc4f61
davidsurosegoro <83533489+davidsurosegoro@users.noreply.github.com>
2024-01-26 11:46:34 +0700

    change port setting

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const dotenv     = require('dotenv');
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.server_port || 3000;
 
 app.use(bodyParser.json());
 
